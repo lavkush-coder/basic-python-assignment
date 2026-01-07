7d2369fd0d0499eb38806944906810d64f53abf5
Lavkush Prajapati <lavkushprajapati32186@gmail.com>
2026-01-07 12:52:44 +0530

    Add files via upload

---
+++ mathmatical_operations.py
@@ -0,0 +1,14 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+# addition
+addition = a+b
+print("addition is :",addition)
+#substraction
+subtraction = a-b
+print("subtraction is :",subtraction)
+#multiplication
+multiplication = a*b
+print("multiplication is :",multiplication)
+#division
+division = a/b
+print("division is :",division)
---
+++ personalized_greeting.py
@@ -0,0 +1,4 @@
+name = str(input("Enter your first name:"))
+surname = str(input("Enter your surname:"))
+
+print("Hello, "+ name+" "+ surname+"!" + " Welcome to the Python program.")
